d9bac83a009f8ffbefa0665ed9a519c023f9a3e9
Pinto <marcos.pinto@accenture.com>
2017-04-24 19:23:04 -0300

    um passinho

---
+++ angularjs-html/js/controllers/FotosController.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope){	
+	$scope.nome = "";
+	$scope.foto = {
+		titulo: 'Leão',
+		url:'http://pt.seaicons.com/wp-content/uploads/2015/07/Young-Lion-icon.png'
+	};
+});
